382edc114b3e5d17f80490cd43d14577eb852668
Shane Clancy <shaneclancy3@gmail.com>
2019-12-23 13:10:44 -0800

    Added base files to repo. Started some class development for Tiles and game.

---
+++ src/index.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div class="app">
+                <Header />
+                <Game />
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="nav-component">Info</div>
+                <div className="nav-component">Help</div>
+                <div className="nav-component">Documentation</div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <Board /> 
+            </div>
+        )
+    }
+}
+
+class Board extends React.Component {
+
+    renderSquares(x,y) {
+        return <Squares x={x} y={y} />
+    }
+
+    renderChoices() {
+        return <Choices />
+    }
+
+    render() {
+        return  (
+            <div className="content-container">
+                <div className="game-board">
+                    <div className="board-row">
+                        {this.renderSquares(0,0)}
+                        {this.renderSquares(0,1)}
+                        {this.renderSquares(0,2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquares(1,0)}
+                        {this.renderSquares(1,1)}
+                        {this.renderSquares(1,2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquares(2,0)}
+                        {this.renderSquares(2,1)}
+                        {this.renderSquares(2,2)}
+                    </div>
+                </div>
+                <div className="choices">
+                    {this.renderChoices()}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Squares extends React.Component {
+
+    renderTile(x,y) {
+        return <Tile x={x} y={y} />
+    }
+
+    render() {
+        return (
+            <div className="squares-collection">
+                <div className="squares-row">
+                    {this.renderTile(0,0)}
+                    {this.renderTile(0,1)}
+                    {this.renderTile(0,2)}
+                </div>
+                <div className="squares-row">
+                    {this.renderTile(1,0)}
+                    {this.renderTile(1,1)}
+                    {this.renderTile(1,2)}
+                </div>
+                <div className="squares-row">
+                    {this.renderTile(2,0)}
+                    {this.renderTile(2,1)}
+                    {this.renderTile(2,2)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Tile extends React.Component {
+    render() {
+        const value = Math.floor(Math.random() * 9) + 1;
+        return (
+            <button className='tile'>
+                {value}
+            </button>
+        );
+    }
+}
+
+class Choice extends React.Component {
+    render() {
+        return (
+            <button className="choice">
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+class Choices extends React.Component {
+    renderChoice(i) {
+        return <Choice value={i} />
+    }
+
+    render() {
+        return (
+            <div className="choices-container">
+                {this.renderChoice(1)}
+                {this.renderChoice(2)}
+                {this.renderChoice(3)}
+                {this.renderChoice(4)}
+                {this.renderChoice(5)}
+                {this.renderChoice(6)}
+                {this.renderChoice(7)}
+                {this.renderChoice(8)}
+                {this.renderChoice(9)}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
