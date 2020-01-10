0407374b6afb0c03c47032dc4fe52a40a418a05e
Shane Clancy <clancys@oregonstate.edu>
2020-01-09 19:28:57 -0800

    Updates

---
+++ precache-manifest.f7d66191c21c9c57c3ce29facf62982a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af05d813bb49135411ab66dcd55e3b63",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "2d0509f001c861cc9637",
+    "url": "/sudoku/static/css/main.bee31447.chunk.css"
+  },
+  {
+    "revision": "1df402edc89ea48dd934",
+    "url": "/sudoku/static/js/2.0ce57fc3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sudoku/static/js/2.0ce57fc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d0509f001c861cc9637",
+    "url": "/sudoku/static/js/main.c7d38d3b.chunk.js"
+  },
+  {
+    "revision": "f548a3de86b8e0db7272",
+    "url": "/sudoku/static/js/runtime-main.e541fa6a.js"
+  }
+]);
